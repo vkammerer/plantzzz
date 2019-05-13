6d187608f45ded7f3aa5bbb87aa7831dddc5c8ad
Vincent Kammerer <vkammerer@gmail.com>
2019-05-14 02:24:35 +1200

    Improves rem definition

--- src/utils/vh.js
+++ src/utils/vh.js
@@ -1,11 +1,13 @@
 export const initCorrectVh = () => {
-  // We listen to the resize event
-  window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-  });
+  const setVh = () => {
+    const vh = window.innerHeight * 0.01;
+    const vw = window.innerWidth * 0.01;
+    const rem = vw / vh < 0.75 ? vw * 5 : vh * 5 * 0.75;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+    document.documentElement.style.setProperty("--rem", `${rem}px`);
+  };
 
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-}
+  // We listen to the resize event
+  window.addEventListener("resize", setVh);
+  setVh();
+};
