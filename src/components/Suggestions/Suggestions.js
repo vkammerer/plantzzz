8539bc3e0aac64f0cb8eb79db756083892d62e91
Vincent <vkammerer@gmail.com>
2020-03-17 10:35:39 +1300

    Adds way to view correct answer when wrong answer is given

--- src/components/Suggestions/Suggestions.js
+++ src/components/Suggestions/Suggestions.js
@@ -3,9 +3,10 @@ import classnames from "classnames";
 
 import "./Suggestions.css";
 
-const Suggestion = ({ suggestion, handleUserAnswer, isSelected }) => {
+const Suggestion = ({ suggestion, handleUserAnswer, isCorrect, isSelected }) => {
   const thisClass = classnames("Suggestion", {
     Suggestion_selected: isSelected,
+    Suggestion_correct: isCorrect
   });
   return (
     <div role="button" className={thisClass} onClick={() => handleUserAnswer(suggestion)}>
@@ -19,7 +20,7 @@ const Suggestions = ({ question, handleUserAnswer }) => {
     Suggestions_botanical: question.type === "botanicalName",
     Suggestions_common: question.type === "commonName",
     Suggestions_true: question.userAnswer && question.userAnswer === question.correctAnswer,
-    Suggestions_false: question.userAnswer && question.userAnswer !== question.correctAnswer,
+    Suggestions_false: question.userAnswer && question.userAnswer !== question.correctAnswer
   });
   return (
     <div className={thisClass}>
@@ -28,6 +29,7 @@ const Suggestions = ({ question, handleUserAnswer }) => {
           key={s}
           suggestion={s}
           handleUserAnswer={handleUserAnswer}
+          isCorrect={question.correctAnswer === s}
           isSelected={question.userAnswer === s}
         />
       ))}
